26e99ef951bcff8ad951a2c4c0dd2216cc779ed2
Nina Mori <dr.ballerina@icloud.com>
2021-12-12 15:00:22 +0900

    Initial Commit

---
+++ PhrasePocket/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhrasePocket
+//
+//  Created by 森 仁奈 on 2021/12/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
